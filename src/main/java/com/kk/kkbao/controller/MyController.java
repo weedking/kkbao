5f9ba640c74e638d884a934ff9f020077995adb1
weedking <260815998@qq.com>
2019-01-28 14:37:54 +0800

    可以正确写入数据库，注意url带有下划线。

--- src/main/java/com/kk/kkbao/controller/MyController.java
+++ src/main/java/com/kk/kkbao/controller/MyController.java
@@ -28,25 +28,25 @@ public class MyController {
     @CrossOrigin
     public SUser addUser( @RequestParam(value = "name",required = false) String name,
                           @RequestParam(value = "gonghao",required = false) String gonghao,
-                          @RequestParam(value = "userCode",required = false) String userCode,
-                          @RequestParam(value = "roleId",required = false) Integer roleId,
-                          @RequestParam(value = "roleName",required = false) String roleName,
+                          @RequestParam(value = "user_code",required = false) String user_code,
+                          @RequestParam(value = "role_id",required = false) Integer role_id,
+                          @RequestParam(value = "role_name",required = false) String role_name,
                           @RequestParam(value = "phone",required = false) String phone,
-                          @RequestParam(value = "openId",required = false) String openId,
-                          @RequestParam(value = "keshiId",required = false) String keshiId,
-                          @RequestParam(value = "keshiName",required = false) String keshiName){
+                          @RequestParam(value = "open_id",required = false) String open_id,
+                          @RequestParam(value = "keshi_id",required = false) String keshi_id,
+                          @RequestParam(value = "keshi_name",required = false) String keshi_name){
         SUser record = new SUser();
         record.setName(name);
         record.setGonghao(gonghao);
-        record.setUserCode(userCode);
-        record.setRoleId(roleId);
-        record.setRoleName(roleName);
+        record.setUserCode(user_code);
+        record.setRoleId(role_id);
+        record.setRoleName(role_name);
         record.setPhone(phone);
-        record.setOpenId(openId);
-        record.setKeshiId(keshiId);
-        record.setKeshiName(keshiName);
+        record.setOpenId(open_id);
+        record.setKeshiId(keshi_id);
+        record.setKeshiName(keshi_name);
         this.userService.addUser(record);
-        System.out.println(name+gonghao+userCode+roleId+roleName+phone+keshiId+keshiName);
+//        System.out.println(name+gonghao+user_code+role_id+role_name+phone+keshi_id+keshi_name);
         return record;
     }
 
